e17d834952edadb63a01508cfdcad9c8155a92ee
Vlad <vladifoki@gmail.com>
2023-04-03 15:48:36 +0300

    DZ2

--- src/Main.java
+++ src/Main.java
@@ -9,68 +9,67 @@ public class Main {
           После ввода нуля, показать на экран количество чисел, которые были введены,
           их общую сумму и среднее арифметическое.*/
 
-        int Summ = 0;
-        int Kolvo = 0;
+        int summ = 0;
+        int kolvo = 0;
         System.out.println("\nВведите желаемые числа. Когда захотите закончить, то введите 0");
-        int Chislo = in.nextInt();
-        while (Chislo != 0){
-            Summ += Chislo;
-            Kolvo += 1;
-            Chislo = in.nextInt();
+        int chislo = in.nextInt();
+        while (chislo != 0){
+            summ += chislo;
+            kolvo += 1;
+            chislo = in.nextInt();
         }
-        System.out.println("Количество введённых чисел:" + Kolvo);
-        System.out.println("Сумма введённых чисел:" + Summ);
-        System.out.println("Среднее арифмитическое введённых чисел:" + ((float) Summ / Kolvo));
+        System.out.println("Количество введённых чисел:" + kolvo);
+        System.out.println("Сумма введённых чисел:" + summ);
+        System.out.println("Среднее арифмитическое введённых чисел:" + ((float) summ / kolvo));
 
         /*2.Написать программу, выводящую факториал числа (произведение чисел от 1 одного до n), заданного с клавиатуры.*/
 
         System.out.println("\nВведите число, факториал которого выхотите найти.\n");
-        int Number = in.nextInt();
-        int Fuck = 1;
-        for (int i = Number; i > 0; i--){
-            Fuck *= i;
+        int number = in.nextInt();
+        int fuck = 1;
+        for (int i = 1; i <= number; i++){
+            fuck *= i;
         }
-        System.out.println("Факториал задонного числа:" + Fuck);
+        System.out.println("Факториал задонного числа:" + fuck);
 
         /*3.Написать программу, которая выводит на экран значение функции y = 5*x2-3*x-10 в диапазоне от -4 до 4,
         с шагом изменения аргумента в 0.5.*/
 
-        for (double i = -4; i <= 4; i += 0.5){
+        for (float i = -4; i <= 4; i += 0.5f){
             System.out.println("\n Значение функции в точке " + i + ": "+ (5*Math.pow(i,2)-3*i-10));
         }
 
         /*4.Вводится действительное х и натуральное n. Вычислить: sin x + sin x в квадрате + ... sin x в степени n.*/
 
-        float X = in.nextFloat();
-        int N = in.nextInt();
-        float Summa = 0;
-        for (int i = 1; i <= N; i++){
-            Summa += Math.pow(Math.sin(X),i);
+        float x = in.nextFloat();
+        int n = in.nextInt();
+        float summa = 0;
+        for (int i = 1; i <= n; i++){
+            summa += Math.pow(Math.sin(x),i);
         }
-        System.out.println("\nВывод: " + Summa);
+        System.out.println("\nВывод: " + summa);
 
         /*5.Вводится натуральное число. Найти сумму четных цифр, входящих в его состав.*/
 
         System.out.println("\nВведите натурально число: ");
-        int Natur = in.nextInt();
-        int Summchet = 0;
-        while (Natur > 0 ){
-            if (Natur % 2 == 0)
-                Summchet += (Natur % 10);
-            Natur /= 10;
+        int natur = in.nextInt();
+        int summchet = 0;
+        while (natur > 0 ){
+            if (natur % 2 == 0)
+                summchet += (natur % 10);
+            natur /= 10;
         }
-        System.out.println("\nСумма четных цифр числа: " + Summchet);
+        System.out.println("\nСумма четных цифр числа: " + summchet);
 
         /*6.Вывести на экран ряд чисел Фибоначчи, состоящий из n элементов.*/
 
-        int N6 = in.nextInt();
-        int F1 = 0;
-        int F2 = 1;
-        while (N6 >= 0){
-            System.out.print(F2 + " ");
-            F2 = F2 + F1;
-            F1 = F2 - F1;
-            N6 -= 1;
+        int n6 = in.nextInt();
+        int f1 = 0;
+        int f2 = 1;
+        for (int i = 1; i <= n6; i++){
+            System.out.print(f2 + " ");
+            f2 = f2 + f1;
+            f1 = f2 - f1;
         }
 
         /*7.Вывести на экран таблицу умножения в виде таблицы*/
@@ -89,25 +88,25 @@ public class Main {
             /* 8.Бизнессмен взял ссуду М тысяч рублей в банке под 20% годовых.
             Через сколько лет его долг превысит S тысяч рублей, если за это время он не будет отдавать долг.*/
 
-        float M = in.nextFloat();
-        float S = in.nextFloat();
-        int Let = 0;
-        while (M <= S){
-            Let += 1;
-            S -= 0.2f * M;
+        float m = in.nextFloat();
+        float s = in.nextFloat();
+        int let = 0;
+        while (m <= s){
+            let += 1;
+            s -= 0.2f * s;
         }
-        System.out.println(Let);
+        System.out.println(let);
 
         /*9.Найти сумму n-го количества элементов ряда 1, -0.5, 0.25, -0.125, …*/
 
-        int N9 = in.nextInt();
-        float Summ9 = 0;
-        float Element = 1;
-        for ( int i = 1; i <= N9; i++){
-            Summ9 += Element;
-            Element *= -0.5f;
+        int n9 = in.nextInt();
+        float summ9 = 0;
+        float element = 1;
+        for ( int i = 1; i <= n9; i++){
+            summ9 += element;
+            element *= -0.5f;
         }
-        System.out.println(Summ9);
+        System.out.println(summ9);
 
         /*10.Напечатать все четырехзначные числа, в десятичной записи которых нет двух одинаковых цифр.*/
 
@@ -150,42 +149,33 @@ public class Main {
                 |  Й й  ||  Ф ф  ||  Я я  |
                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
 
-        char Bukva = 1040;
-        for (int i = 6; i > 0; i--){
+        char bukva = 1040;
+        for (int i = 0; i < 6; i++){
             System.out.println("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-            for (int j = 2; j > 0; j--){
-                System.out.print("|  "+ Bukva + " " +(char)(Bukva + 32) + "  |");
-                Bukva += 10;
+            for (int j = 0; j < 2; j++){
+                System.out.print("|  "+ bukva + " " +(char)(bukva + 32) + "  |");
+                bukva += 10;
             } for (int j = 1; j > 0; j--){
-                Bukva += 1;
-                System.out.print("|  "+ Bukva + " " +(char)(Bukva + 32) + "  |");
+                bukva += 1;
+                System.out.print("|  "+ bukva + " " +(char)(bukva + 32) + "  |");
             }
-            Bukva -= 20;
-        }for (int i = 1; i > 0; i--) {
+            bukva -= 20;
+        }   System.out.println("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+                System.out.print("|  "+ (char) 1025 + " " +(char) 1105 + "  |");
+                bukva = 1056;
+            for (int j = 0; j < 2; j++){
+                System.out.print("|  "+ bukva + " " +(char)(bukva + 32) + "  |");
+                bukva += 11;
+            }
+         bukva -= 32;
+        for (int i = 0; i < 4; i++){
             System.out.println("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-            for (int j = 1; j > 0; j--) {
-                Bukva = 1025;
-                System.out.print("|  " + Bukva + " ");
-                Bukva = 1105;
-                System.out.print(Bukva + "  |");
+            for (int j = 3; j > 0; j--){
+                System.out.print("|  "+ bukva + " " +(char)(bukva + 32) + "  |");
+                bukva += 11;
             }
-                Bukva = 1056;
-            for (int j = 2; j > 0; j--){
-                System.out.print("|  "+ Bukva + " " +(char)(Bukva + 32) + "  |");
-                Bukva += 11;
-            }
-        } Bukva -= 32;
-        for (int i = 4; i > 0; i--){
-            System.out.println("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-            for (int j = 2; j > 0; j--){
-                System.out.print("|  "+ Bukva + " " +(char)(Bukva + 32) + "  |");
-                Bukva += 11;
-            } for (int j = 1; j > 0; j--){
-                System.out.print("|  "+ Bukva + " " +(char)(Bukva + 32) + "  |");
-            }
-            Bukva -= 21;
+            bukva -= 32;
         }
-        for (int i = 1; i > 0; i--)
             System.out.println("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 
         /* 2*) Валентина прогуляла лекцию по математике.
@@ -198,15 +188,15 @@ public class Main {
 
         for (int i = 1; i <= Math.sqrt(23436); i++){
             if (23436 % i == 0)
-                System.out.print(i + " " + 23436 / i);
+                System.out.print(i + " " + 23436 / i + " ");
         }System.out.println("");
         for (int i = 1; i <= Math.sqrt(190187200); i++){
             if (190187200 % i == 0)
-                System.out.print(i + " " + 190187200 / i);
+                System.out.print(i + " " + 190187200 / i + " ");
         }System.out.println("");
         for (long i = 1; i <= Math.sqrt(380457890232L); i++){
             if (380457890232L % i == 0)
-                System.out.print(i + " " + 380457890232L / i);
+                System.out.print(i + " " + 380457890232L / i + " ");
         }
     }
 }
