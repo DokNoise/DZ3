5d9e54c67971fe0a28624017c6e7ae0839b68f7d
Vlad <vladifoki@gmail.com>
2023-04-03 15:49:31 +0300

    DZ2

--- src/Main.java
+++ src/Main.java
@@ -170,7 +170,7 @@ public class Main {
          bukva -= 32;
         for (int i = 0; i < 4; i++){
             System.out.println("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-            for (int j = 3; j > 0; j--){
+            for (int j = 0; j < 3; j++){
                 System.out.print("|  "+ bukva + " " +(char)(bukva + 32) + "  |");
                 bukva += 11;
             }
